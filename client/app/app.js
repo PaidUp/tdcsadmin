98e78e88335db65a212aca389ec2c09fcd3c761b
Ricardo Lara <riclara@gmail.com>
2016-02-24 23:42:51 -0600

    update Facebook app id

--- client/app/app.js
+++ client/app/app.js
@@ -27,7 +27,7 @@ angular.module('convenienceApp', [
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
-    FacebookProvider.init('717631811625048');
+    FacebookProvider.init('499580560213861');
     $httpProvider.interceptors.push('authInterceptor');
 
     $httpProvider.defaults.cache = false;
